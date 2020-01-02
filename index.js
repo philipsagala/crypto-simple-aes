5426baea8d72d63d2dbd011e08405dd778bd02f8
philipsagala <philipsagala@gmail.com>
2020-01-02 13:28:43 +0700

    Update index.js
    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const crypto = require('crypto');
-const algorithm = 'aes-256-cbc';
-const key = 'f57ydsvior89dku53xpqc401nyl3cgsj';
+const crypto = require('crypto')
+const algorithm = 'aes-256-cbc'
+const key = 'f57ydsvior89dku53xpqc401nyl3cgsj'
 const iv = 'aw90rela942f65u2'
 
 exports.encrypt = function(text) {
-    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     return encrypted.toString('hex')
@@ -12,7 +12,7 @@ exports.encrypt = function(text) {
 
 exports.decrypt = function(text) {
     let encryptedText = Buffer.from(text, 'hex');
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
